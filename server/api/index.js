d1b6dbdd20022b8d414b8627df1c724cb599f620
smanabat <sheldon@ducklabs.ca>
2020-05-11 10:34:58 -0600

    add v2 routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const apiRouter = express.Router()
+const bodyParser = require('body-parser')
+
+const Router = express.Router
 
 const database = require('../database')
 
 const setupV1Routes = (apiRouter) => {
+
+  // Controller Functions
   function findAllPosts(request, response) {
     let allPosts = database.findAllPosts()
     response.send(allPosts)
@@ -15,13 +19,43 @@ const setupV1Routes = (apiRouter) => {
     response.send(200)
   }
 
-  apiRouter.get('/posts', findAllPosts)
-  apiRouter.post('/addPost', addNewPost)
+  // Routing
+  const v1Router = Router()
+  v1Router.get('/posts', findAllPosts)
+  v1Router.post('/addPost', addNewPost)
+
+  apiRouter.use('/v1', v1Router)
+};
+
+const setupV2Routes = (apiRouter) => {
+
+  // Controller Functions
+  function findAllPosts(request, response) {
+    let allPosts = database.findAllPosts()
+    response.send(allPosts)
+  }
+
+  function addNewPost(request, response) {
+    console.log('saving post', request.body)
+    database.addPost(request.body)
+    response.send(200)
+  }
+
+  // Middleware
+  const textParser = bodyParser.text()
+
+  // Routing
+  const router = Router()
+  router.get('/posts', findAllPosts)
+  router.post('/addPost', textParser, addNewPost)
+
+  apiRouter.use('/v2', router)
 };
 
 
-setupV1Routes(apiRouter);
-// setupV2Routes(apiRouter);
 
+const apiRouter = Router()
+setupV1Routes(apiRouter)
+setupV2Routes(apiRouter)
 
 module.exports = apiRouter
