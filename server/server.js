7ed0f1afaa8f7213c88adb9a73d9e52ba059d2ec
smanabat <sheldon@ducklabs.ca>
2020-05-11 09:41:19 -0600

    adding day3 static page

--- server/server.js
+++ server/server.js
@@ -5,7 +5,10 @@ const app = express()
 
 app.use(express.json())
 app.use('/api', api)
+
 app.use('/', express.static('../web1'))
+
+app.use('/day3', express.static('../web3'))
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Server listening at ${port}`))
