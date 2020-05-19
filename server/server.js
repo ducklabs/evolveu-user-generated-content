7c965963d6816ad7578961b758c8afe9129db67e
Tony Enerson <tony@enerson.ca>
2020-05-19 15:07:19 -0600

    Working up basic form

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 app.use('/api', api)
 
-app.use('/', express.static('../web1'))
+app.use('/day2', express.static('../web2'))
 app.use('/day3', express.static('../web3'))
 
 const port = process.env.PORT || 3000
