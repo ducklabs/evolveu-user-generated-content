d619df615c4f0f9b296b256c17241d4bb1671ca3
Tony Enerson <tony@enerson.ca>
2020-05-19 15:12:14 -0600

    make index page a bit more helpful

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use('/api', api)
-
+app.use('/', express.static('../web1'))
 app.use('/day2', express.static('../web2'))
 app.use('/day3', express.static('../web3'))
 
